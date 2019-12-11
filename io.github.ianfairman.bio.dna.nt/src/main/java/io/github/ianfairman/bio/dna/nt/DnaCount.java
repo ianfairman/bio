9d760de4e3af118804ed51394612337822862249
Ian Fairman <ian.fairman@gmail.com>
2019-12-11 12:13:05 +0000

    test: DNA nucleotide counts.

--- io.github.ianfairman.bio.dna.nt/src/main/java/io/github/ianfairman/bio/dna/nt/DnaCount.java
+++ io.github.ianfairman.bio.dna.nt/src/main/java/io/github/ianfairman/bio/dna/nt/DnaCount.java
@@ -1,7 +1,9 @@
 package io.github.ianfairman.bio.dna.nt;
 
 import java.util.AbstractMap;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 import static java.util.Objects.requireNonNull;
 import java.util.Set;
 import static java.util.stream.Collectors.joining;
@@ -9,23 +11,49 @@ import java.util.stream.Stream;
 
 public class DnaCount extends AbstractMap<DnaNucleotide, Long> {
 
-  private final Map<DnaNucleotide, Long> counts;
+    private final Map<DnaNucleotide, Long> counts;
 
-  public DnaCount(Map<DnaNucleotide, Long> counts) {
-    this.counts = requireNonNull(counts);
-  }
-  public double getGcContent() {
-    return (double) (counts.getOrDefault(DnaNucleotide.GUANINE, 0L) + counts.getOrDefault(DnaNucleotide.CYTOSINE, 0L))
-            / counts.values().stream().mapToDouble(Long::doubleValue).sum();
-  }
+    public DnaCount(Map<DnaNucleotide, Long> counts) {
+        this.counts = new HashMap<>(requireNonNull(counts));
+    }
 
-  @Override
-  public Set<Entry<DnaNucleotide, Long>> entrySet() {
-    return counts.entrySet();
-  }
+    public double getGcContent() {
+        return (double) (counts.getOrDefault(DnaNucleotide.GUANINE, 0L) + counts.getOrDefault(DnaNucleotide.CYTOSINE, 0L))
+                / counts.values().stream().mapToDouble(Long::doubleValue).sum();
+    }
 
-  @Override
-  public String toString() {
-    return Stream.of(DnaNucleotide.values()).map(n -> Long.toString(counts.get(n))).collect(joining(" "));
-  }
+    @Override
+    public Set<Entry<DnaNucleotide, Long>> entrySet() {
+        return counts.entrySet();
+    }
+
+    @Override
+    public String toString() {
+        return Stream.of(DnaNucleotide.values()).map(n -> Long.toString(counts.get(n))).collect(joining(" "));
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 47 * hash + Objects.hashCode(this.counts);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final DnaCount other = (DnaCount) obj;
+        if (!Objects.equals(this.counts, other.counts)) {
+            return false;
+        }
+        return true;
+    }
 }
---
+++ io.github.ianfairman.bio.dna.nt/src/test/java/io/github/ianfairman/bio/dna/nt/DnaSequenceTest.java
@@ -0,0 +1,24 @@
+package io.github.ianfairman.bio.dna.nt;
+
+import java.util.Map;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author Ian Fairman <ian.fairman@gmail.com>
+ */
+public class DnaSequenceTest {
+    
+    @Test
+    public void shouldCalculateCorrectNucleotideCounts() {
+        String input = "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC";
+        DnaCount expectedCount = new DnaCount(Map.of(
+             DnaNucleotide.ADENINE, 20L,
+             DnaNucleotide.CYTOSINE, 12L,
+             DnaNucleotide.GUANINE, 17L,
+             DnaNucleotide.THYMINE, 21L));
+        DnaSequence dnaSequence = DnaSequence.parse(input);
+        assertEquals(expectedCount, dnaSequence.counts());
+    }
+}
